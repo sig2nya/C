bc382a3d3ce618491522909d2ee701188f6e1786
HongMinSik <starwalk7058@naver.com>
2022-07-12 16:24:46 +0900

    'struct'

--- C언어/Hello/코딩도장/Main.c
+++ C언어/Hello/코딩도장/Main.c
@@ -1,6 +1,16 @@
 ﻿#include<stdio.h>
 
 int main() {
-	printf("Hello, World\n");
+	// 1. Hello, World!
+	printf("Hello, World!\n");
+
+	// 2. 서식 지정자
+	printf("%s\n", "Hello, World!");
+	printf("%s %s\n", "Hello", "1234");
+	printf("%s %s\n", "Hello,", "1234");
+	printf("%s, %s\n", "Hello", "1234");
+	printf("%s, %s\n", "Hello,", "1234");
+	printf("%s%s\n", "Hello", "1234");
+
 	return 0;
 }
